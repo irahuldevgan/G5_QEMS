2ade125d1fcc8e2255f628e579175321d06333ac
irahuldevgan <rahuldevgan572@gmail.com>
2021-04-09 11:04:47 -0700

    adding files in repo

---
+++ src/quiz/or/exam/management/system/QuizOrExamManagementSystem.java
@@ -0,0 +1,14 @@
+
+package quiz.or.exam.management.system;
+
+
+public class QuizOrExamManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
